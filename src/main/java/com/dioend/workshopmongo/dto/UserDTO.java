742ad1d987e2b7ce97f6c0dd1e80f5458f92d70f
Diovane Pacheti <diovane.pacheti@l2code.com.br>
2023-02-02 23:01:01 -0300

    :sparkles: create class DTO

---
+++ src/main/java/com/dioend/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.dioend.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.dioend.workshopmongo.domain.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String name;
+	private String email;
+		
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+}
